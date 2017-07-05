b42417e6dd29e0cc6790689df7541aeeb96fce8f
UbuntuLover555 <ubuntulover@tutanota.com>
2017-07-05 09:49:15 +0530

    Add files via upload

---
+++ wget.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+echo Bash CLI Script 1.0
+echo Developed By linuxlover555
+echo Input DDL [Direct Download Link]
+read download
+wget $download
+echo File Saved In Current Directory
+
+
+
